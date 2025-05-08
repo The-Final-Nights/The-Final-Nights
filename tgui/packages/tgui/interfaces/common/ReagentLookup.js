a369bee33ee2a2c4eeeb80823f5469d4cd81ab5d
XeonMations <myvilnius@gmail.com>
2025-05-08 16:00:24 +0300

    Revert "aaa"
    This reverts commit e07d0747435d6d139169b74e2648027df0d481a5.

--- tgui/packages/tgui/interfaces/common/ReagentLookup.js
+++ tgui/packages/tgui/interfaces/common/ReagentLookup.js
@@ -1,12 +1,15 @@
-import { Box, Button, Icon, LabeledList } from 'tgui-core/components';
-
 import { useBackend } from '../../backend';
+import { Box, Button, Icon, LabeledList } from '../../components';
 
-export const ReagentLookup = (props) => {
+export const ReagentLookup = (props, context) => {
   const { reagent } = props;
-  const { act } = useBackend();
+  const { act } = useBackend(context);
   if (!reagent) {
-    return <Box>No reagent selected!</Box>;
+    return (
+      <Box>
+        No reagent selected!
+      </Box>
+    );
   }
 
   return (
@@ -22,10 +25,11 @@ export const ReagentLookup = (props) => {
           tooltipPosition="left"
           onClick={() => {
             Byond.command(`wiki Guide_to_chemistry#${reagent.name}`);
-          }}
-        />
+          }} />
       </LabeledList.Item>
-      <LabeledList.Item label="Description">{reagent.desc}</LabeledList.Item>
+      <LabeledList.Item label="Description">
+        {reagent.desc}
+      </LabeledList.Item>
       <LabeledList.Item label="pH">
         <Icon name="circle" mr={1} color={reagent.pHCol} />
         {reagent.pH}
@@ -33,12 +37,16 @@ export const ReagentLookup = (props) => {
       <LabeledList.Item label="Properties">
         <LabeledList>
           {!!reagent.OD && (
-            <LabeledList.Item label="Overdose">{reagent.OD}u</LabeledList.Item>
+            <LabeledList.Item label="Overdose">
+              {reagent.OD}u
+            </LabeledList.Item>
           )}
           {reagent.addictions[0] && (
             <LabeledList.Item label="Addiction">
-              {reagent.addictions.map((addiction) => (
-                <Box key={addiction}>{addiction}</Box>
+              {reagent.addictions.map(addiction => (
+                <Box key={addiction}>
+                  {addiction}
+                </Box>
               ))}
             </LabeledList.Item>
           )}
@@ -56,12 +64,9 @@ export const ReagentLookup = (props) => {
                 tooltip="This reagent will partially convert into this when the purity is above the Inverse purity on consumption."
                 tooltipPosition="left"
                 content={reagent.impureReagent}
-                onClick={() =>
-                  act('reagent_click', {
-                    id: reagent.impureId,
-                  })
-                }
-              />
+                onClick={() => act('reagent_click', {
+                  id: reagent.impureId,
+                })} />
             </LabeledList.Item>
           )}
           {reagent.inverseReagent && (
@@ -71,12 +76,9 @@ export const ReagentLookup = (props) => {
                 content={reagent.inverseReagent}
                 tooltip="This reagent will convert into this when the purity is below the Inverse purity on consumption."
                 tooltipPosition="left"
-                onClick={() =>
-                  act('reagent_click', {
-                    id: reagent.inverseId,
-                  })
-                }
-              />
+                onClick={() => act('reagent_click', {
+                  id: reagent.inverseId,
+                })} />
             </LabeledList.Item>
           )}
           {reagent.failedReagent && (
@@ -86,35 +88,39 @@ export const ReagentLookup = (props) => {
                 tooltip="This reagent will turn into this if the purity of the reaction is below the minimum purity on completion."
                 tooltipPosition="left"
                 content={reagent.failedReagent}
-                onClick={() =>
-                  act('reagent_click', {
-                    id: reagent.failedId,
-                  })
-                }
-              />
+                onClick={() => act('reagent_click', {
+                  id: reagent.failedId,
+                })} />
             </LabeledList.Item>
           )}
         </LabeledList>
-        {reagent.isImpure && <Box>This reagent is created by impurity.</Box>}
-        {reagent.deadProcess && <Box>This reagent works on the dead.</Box>}
-        {!reagent.failedReagent &&
-          !reagent.inverseReagent &&
-          !reagent.impureReagent && (
-            <Box>This reagent has no impure reagents.</Box>
-          )}
+        {reagent.isImpure && (
+          <Box>
+            This reagent is created by impurity.
+          </Box>
+        )}
+        {reagent.deadProcess && (
+          <Box>
+            This reagent works on the dead.
+          </Box>
+        )}
+        {!reagent.failedReagent
+          && !reagent.inverseReagent
+          && !reagent.impureReagent && (
+          <Box>
+            This reagent has no impure reagents.
+          </Box>
+        )}
       </LabeledList.Item>
       <LabeledList.Item>
         <Button
           icon="flask"
           mt={2}
-          content={'Find associated reaction'}
+          content={"Find associated reaction"}
           color="purple"
-          onClick={() =>
-            act('find_reagent_reaction', {
-              id: reagent.id,
-            })
-          }
-        />
+          onClick={() => act('find_reagent_reaction', {
+            id: reagent.id,
+          })} />
       </LabeledList.Item>
     </LabeledList>
   );
